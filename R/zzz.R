f1eff9ef6a8c816aaa9ddfec50f610f89a03ce9f
mjcasy <m.j.casey@soton.ac.uk>
2021-04-28 17:05:21 +0100

    Source python via system.file

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,10 @@
-# .onLoad <- function(libname, pkgname){
-#   reticulate::source_python("splitCells.py")
-#   reticulate::source_python("Heterogeneity.py")
-# }
+.onLoad <- function(libname, pkgname){
+
+  path <- system.file("python", package = "scEC")
+
+  reticulate::source_python(paste0(path, "/splitCells.py"))
+  reticulate::source_python(paste0(path, "/Heterogeneity.py"))
+
+  reticulate::import("scipy", delay_load = TRUE)
+  reticulate::import("numpy", delay_load = TRUE)
+}
